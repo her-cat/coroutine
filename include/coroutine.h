d7fa096dcc866f2b6c9f5cfe99efa64422da3dab
her-cat <i@her-cat.com>
2021-01-07 16:14:12 +0800

    协程恢复、跳转、关闭、初始化等函数

--- include/coroutine.h
+++ include/coroutine.h
@@ -1,58 +1,92 @@
 #ifndef COROUTINE_H
 #define COROUTINE_H
 
+#include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
 #include "builtin.h"
 
-#define COROUTINE_G(x)  GLOBALS_GET(coroutine, x)
+#define COROUTINE_G(x)      GLOBALS_GET(coroutine, x)
+#define coroutine_resume    COROUTINE_G(resume)
 
+/* 协程栈大小 */
+#define COROUTINE_MIN_STACK_SIZE            (128 * 1024)
+#define COROUTINE_RECOMMENDED_STACK_SIZE    (256 * 1024)
+#define COROUTINE_MAX_STACK_SIZE            (16 * 1024 * 1024)
+
+/* 协程id */
+#define COROUTINE_MAX_ID    UINT64_MAX
+#define COROUTINE_MAIN_ID   1
+
+/* 协程状态枚举 */
 typedef enum {
     COROUTINE_STATE_INIT,
     COROUTINE_STATE_READY,
     COROUTINE_STATE_RUNNING,
+    COROUTINE_STATE_WAITING,
+    COROUTINE_STATE_LOCKED,
     COROUTINE_STATE_FINISHED,
     COROUTINE_STATE_DEAD,
 } coroutine_state_t;
 
+/* 协程操作码 */
 typedef enum {
     COROUTINE_OPCODE_NONE,
     COROUTINE_OPCODE_CHECKED,
 } coroutine_opcode_t;
 
+/* 协程回调函数原型 */
 typedef void *(*coroutine_function_t)(void *);
-
+/* 协程类型 */
 typedef struct coroutine_s coroutine_t;
+/* 协程上下文类型 */
+typedef ucontext_t coroutine_context_t;
 
+/* 协程数据结构 */
 struct coroutine_s {
-    uint64_t id;
-    coroutine_state_t state;
-    coroutine_opcode_t opcodes;
-    coroutine_t *from;
-    coroutine_t *previous;
-    void *stack;
-    uint32_t stack_size;
-    coroutine_function_t function;
-    void *context;
-    void *transfer_data;
+    union {
+        coroutine_t *coroutine;
+    } waiter;
+    uint64_t id; /* 协程id */
+    coroutine_state_t state; /* 协程状态 */
+    coroutine_opcode_t opcodes; /* 协程操作码 */
+    coroutine_t *from; /* 协程来源 */
+    coroutine_t *previous; /* 前置协程 */
+    void *stack; /* 自定义栈 */
+    uint32_t stack_size; /* 栈大小 */
+    coroutine_function_t function; /* 协程回调函数 */
+    coroutine_context_t context; /* 协程上下文 */
+    void *transfer_data; /* 传输数据 */
 };
 
 typedef bool_t (*coroutine_resume_t)(coroutine_t *coroutine, void *data, void **retval);
 
+/* 协程全局状态 */
 GLOBALS_STRUCT_BEGIN(coroutine)
-    uint64_t last_id;
-    uint32_t count;
-    uint32_t default_stack_size;
+    uint64_t last_id; /* 最后一个协程id */
+    uint32_t count; /* 协程数量 */
+    uint32_t default_stack_size; /* 默认栈大小 */
     /* coroutines */
-    coroutine_t *current;
-    coroutine_t *main;
-    coroutine_t _main;
+    coroutine_t *current; /* 当前协程指针 */
+    coroutine_t *main; /* 主协程指针 */
+    coroutine_t _main; /* 主协程 */
     /* scheduler */
-    coroutine_t *scheduler;
+    coroutine_t *scheduler; /* 协程调度器 */
     /* functions */
-    coroutine_resume_t resume;
-
+    coroutine_resume_t resume; /* 协程恢复函数指针 */
 GLOBALS_STRUCT_END(coroutine)
 
 extern GLOBALS_DECLARE(coroutine)
+
+bool_t coroutine_runtime_init(void);
+uint32_t coroutine_set_default_stack_size(uint32_t size);
+coroutine_resume_t coroutine_register_resume(coroutine_resume_t resume);
+bool_t coroutine_resume_standard(coroutine_t *coroutine, void *data, void **retval);
+bool_t coroutine_is_resumable(coroutine_t *coroutine);
+bool_t coroutine_is_alive(coroutine_t *coroutine);
+void *coroutine_jump(coroutine_t *coroutine, void *data);
+void coroutine_close(coroutine_t *coroutine);
 
 #endif
--- src/coroutine.c
+++ src/coroutine.c
@@ -1,5 +1,171 @@
 #include "coroutine.h"
-#include <stdio.h>
+
+#define coroutine_context_make(ucontext, function, argc, ...) \
+        makecontext(ucontext, function, argc, ##__VA_ARGS__)
+
+#define coroutine_context_jump(current_ucontext, ucontext) \
+        swapcontext(current_ucontext, ucontext)
+
+GLOBALS_DECLARE(coroutine)
+
+/* 协程运行时初始化 */
+bool_t coroutine_runtime_init(void) {
+    /* 注册协程恢复函数 */
+    coroutine_register_resume(coroutine_resume_standard);
+
+    /* 初始化协程 */
+    coroutine_set_default_stack_size(COROUTINE_RECOMMENDED_STACK_SIZE);
+
+    /* 初始化主协程 */
+    coroutine_t *main_coroutine = &COROUTINE_G(_main);
+    main_coroutine->id = COROUTINE_MAIN_ID;
+    main_coroutine->state = COROUTINE_STATE_RUNNING;
+    main_coroutine->opcodes = COROUTINE_OPCODE_NONE;
+    main_coroutine->from = NULL;
+    main_coroutine->previous = NULL;
+    main_coroutine->stack = NULL;
+    main_coroutine->stack_size = 0;
+    main_coroutine->function = NULL;
+    main_coroutine->transfer_data = NULL;
+    memset(&main_coroutine->context, 0, sizeof(coroutine_t));
+
+    /* 初始化协程全局变量 */
+    COROUTINE_G(last_id) = main_coroutine->id + 1;
+    COROUTINE_G(count) = 1;
+    COROUTINE_G(current) = main_coroutine;
+    COROUTINE_G(main) = main_coroutine;
+    COROUTINE_G(scheduler) = NULL;
+
+    return true;
+}
+
+/* 设置协程默认栈大小 */
+uint32_t coroutine_set_default_stack_size(uint32_t size) {
+    uint32_t origin_size = COROUTINE_G(default_stack_size);
+    COROUTINE_G(default_stack_size) = size;
+    return origin_size;
+}
+
+/* 注册恢复协程的函数，并返回原来的恢复函数 */
+coroutine_resume_t coroutine_register_resume(coroutine_resume_t resume) {
+    coroutine_resume_t origin_resume = coroutine_resume;
+    coroutine_resume = resume;
+    return origin_resume;
+}
+
+/* 恢复协程 */
+bool_t coroutine_resume_standard(coroutine_t *coroutine, void *data, void **retval) {
+    if (!(coroutine->opcodes & COROUTINE_OPCODE_CHECKED)) {
+        if (!coroutine_is_resumable(coroutine)) {
+            return false;
+        }
+    }
+
+    data = coroutine_jump(coroutine, data);
+    if (retval != NULL) {
+        *retval = data;
+    }
+
+    return true;
+}
+
+/* 判断协程是否可恢复 */
+bool_t coroutine_is_resumable(coroutine_t *coroutine) {
+    coroutine_t *current_coroutine = COROUTINE_G(current);
+
+    /* 如果 coroutine 是 current_coroutine 的前置协程 */
+    if (coroutine == current_coroutine->previous) {
+        return true;
+    }
+
+    /* 检查协程状态 */
+    switch (coroutine->state) {
+        case COROUTINE_STATE_READY:
+        case COROUTINE_STATE_WAITING:
+            break;
+        case COROUTINE_STATE_RUNNING:
+            printf("coroutine[%ld] is running\n", coroutine->id);
+            return false;
+        case COROUTINE_STATE_LOCKED:
+            printf("coroutine[%ld] is locked\n", coroutine->id);
+            return false;
+        default:
+            printf("coroutine[%ld] is not available\n", coroutine->id);
+            return false;
+    }
+
+    /* current_coroutine 是否为 coroutine 等待的协程 */
+    if ((coroutine->state & COROUTINE_STATE_WAITING) && current_coroutine != coroutine->waiter.coroutine) {
+        printf("coroutine[%ld] is waiting\n", coroutine->id);
+        return false;
+    }
+
+    return true;
+}
+
+/* 协程是否存活 */
+bool_t coroutine_is_alive(coroutine_t *coroutine) {
+    return coroutine->state < COROUTINE_STATE_LOCKED && coroutine->state > COROUTINE_STATE_READY;
+}
+
+/* 协程跳转 */
+void *coroutine_jump(coroutine_t *coroutine, void *data) {
+    coroutine_t *current_coroutine = COROUTINE_G(current);
+
+    printf("jump to coroutine[%ld]\n", coroutine->id);
+
+    /* 记录 coroutine 是从哪个协程切换过来的 */
+    coroutine->from = current_coroutine;
+    if (current_coroutine->previous == coroutine) {
+        /* 如果是 yield，则更新当前协程的状态为等待中 */
+        if (current_coroutine->state == COROUTINE_STATE_RUNNING) {
+            current_coroutine->state = COROUTINE_STATE_WAITING;
+        }
+        current_coroutine->previous = NULL;
+    } else {
+        /* current_coroutine 成为 coroutine 的前置协程 */
+        coroutine->previous = current_coroutine;
+    }
+
+    /* 将 coroutine 设置为当前正在运行的协程 */
+    COROUTINE_G(current) = coroutine;
+    /* 更新协程状态 */
+    coroutine->state = COROUTINE_STATE_RUNNING;
+    /* 重置操作码 */
+    coroutine->opcodes = COROUTINE_OPCODE_NONE;
+    /* 开始跳转到 coroutine */
+    coroutine->transfer_data = data;
+    coroutine_context_jump(&current_coroutine->context, &coroutine->context);
+    data = current_coroutine->transfer_data;
+    /* 处理来源 */
+    coroutine = current_coroutine->from;
+
+    /* 如果已完成则关闭协程 */
+    if (coroutine->state == COROUTINE_STATE_FINISHED) {
+        coroutine_close(coroutine);
+    }
+
+    return data;
+}
+
+/* 关闭协程 */
+void coroutine_close(coroutine_t *coroutine) {
+    void *stack = coroutine->stack;
+
+    if (stack == NULL) {
+        printf("coroutine[%ld] is unready or closed\n", coroutine->id);
+        return;
+    }
+
+    if (coroutine_is_alive(coroutine)) {
+        printf("coroutine[%ld] should not be active\n", coroutine->id);
+        return;
+    }
+
+    coroutine->state = COROUTINE_STATE_DEAD;
+    coroutine->stack = NULL;
+    free(stack);
+}
 
 int main() {
     printf("hello, coroutine\n");
